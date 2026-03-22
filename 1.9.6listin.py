557c6ea03715bdf234a5979822cb1d2dbdccd0fa
diqanayeva <diqanayevaayka@gmail.com>
2026-03-22 16:13:08 +0400

    list1

---
+++ 1.9.6listin.py
@@ -0,0 +1,13 @@
+ickiler=["kofe","cay","limonad"]
+fast_food=["pizza","burger","kabab"]
+isti_yemekler=["doner","kofte","baliq"]
+salatlar=["salat","meze","sous"]
+yemekler=[
+    
+    ickiler,
+    fast_food,
+    isti_yemekler,
+    salatlar
+]
+
+print ( yemekler [2][1] )
