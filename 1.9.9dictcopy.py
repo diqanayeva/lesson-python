1624d8e97f454f2fe3a884f6390bfe17ee9b65f8
diqanayeva <diqanayevaayka@gmail.com>
2026-03-23 21:00:30 +0400

    copy

---
+++ 1.9.9dictcopy.py
@@ -0,0 +1,12 @@
+melumatlar={
+    'istehsalci': 'Renault',
+    'model' : 'Clio',
+    'il' : 2009,
+
+}
+
+yeni_melumatlar = melumatlar.copy()
+yeni_melumatlar['il']=2010
+
+print(melumatlar)
+   
