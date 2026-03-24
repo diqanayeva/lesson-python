f812cd06ba513ff9b5d4bf123733ea5f608a5da8
diqanayeva <diqanayevaayka@gmail.com>
2026-03-24 23:04:06 +0400

    dict7

---
+++ 1.9.9 dictindict.py
@@ -0,0 +1,13 @@
+murad = {
+    'ad': "Murad",
+    'soyad': "Abbasov"
+}
+camal = {
+    'ad': "Camal",
+    'soyad': "Camalov"
+}
+kisiler = {
+    'murad': murad,
+    'camal': camal
+}
+print ( kisiler['murad']['soyad'])
