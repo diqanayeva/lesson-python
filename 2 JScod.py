d34538788c6b481c88861e1d9178bb1afa6334d7
diqanayeva <diqanayevaayka@gmail.com>
2026-03-31 23:16:50 +0400

    cod

---
+++ 2 JScod.py
@@ -0,0 +1,13 @@
+import json
+
+with open( "products.json", "r", encoding = "utf-8" ) as file:
+     data= json.load( file )
+
+     #print( data )
+print( " Mehsul siyahisi : \n")
+
+for i in data [ "products"]:
+     print(f"ID :     {i [ 'id']    } ")
+     print(f"Ad:      {i ['name']   } ")
+     print(f"Qiymet:  {i ['price']  } ")
+     print(f"Stock:   {i [ 'stock'] } ")
