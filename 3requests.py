8de57c15384e0f88bb0cb651cb564b80c11ab1f4
diqanayeva <diqanayevaayka@gmail.com>
2026-04-18 23:04:29 +0400

    post

--- 3requests.py
+++ 3requests.py
@@ -3,5 +3,14 @@ import requests
 url = "https://jsonplaceholder.typicode.com/posts"
 
 response = requests.get(url)
+data = response.json()
+
 
 print(" postlarimiz:\n")
+
+for post in data[:5]:
+     print(f"ID :   {post [ 'id']   } ")
+     print(f"Title: {post ['title'] } ")
+     print(f"Body:  {post ['body']  }\n ")
+     print("-"*40)
+
