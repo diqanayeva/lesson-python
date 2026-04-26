29b207ac45e3c1a71d6f3528e513f6287acd8d4f
diqanayeva <diqanayevaayka@gmail.com>
2026-04-27 00:01:32 +0400

    return5

--- 10lambda.py
+++ 10lambda.py
@@ -5,3 +5,12 @@ print( topla(4,6) )
 def topla1(a,b): return a + b
 
 print(  topla1(3,2) )
+
+#def topla1(a,b): return a + b
+
+netice = topla1
+
+print( netice( 4,6) )
+
+#ekranaYazmaq=print
+#ekranaYazmaq('Salam')
---
+++ 10lamdaIf.py
@@ -0,0 +1,3 @@
+ededi_yoxla = lambda yash:  yash>=18
+
+print( ededi_yoxla(18) )
