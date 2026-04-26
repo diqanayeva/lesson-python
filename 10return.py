05f7cf9b96602634a6eb12c235e1ed25077c3899
diqanayeva <diqanayevaayka@gmail.com>
2026-04-26 22:59:27 +0400

    return

---
+++ 10return.py
@@ -0,0 +1,14 @@
+def topla(a,b):
+    #a=int(input("Birinci sayini daxil edin:"))
+    #b=int(input("Ikinci sayini daxil edin:"))
+
+    print(a+b)
+
+#topla()
+
+#def vurma():
+    #topla() * topla()
+topla(5,7)
+topla(4,6)
+topla(3,8)
+topla(2,2)
