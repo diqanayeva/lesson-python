c499cf3cb5146c43823d477e1235281d1f6a2472
diqanayeva <diqanayevaayka@gmail.com>
2026-05-03 15:47:17 +0400

    format

---
+++ 11format.py
@@ -0,0 +1,12 @@
+heyvan = 'inek'
+cisim = 'qeder'
+
+
+cumle = "{}  saman yoluna  {}   cixa bilmez.Cunki inek samanlari yeyer."
+
+print(   cumle.format( heyvan, cisim )  )
+
+
+#daha asan formasi
+
+cumle = f"{heyvan}  saman yoluna {cisim} cixa bilmez. Cunki inek samanlari yeyer."
