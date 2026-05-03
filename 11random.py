04efae6d1d2582dd7304a2a0f0416fe2159110e9
diqanayeva <diqanayevaayka@gmail.com>
2026-05-03 16:15:58 +0400

    random

---
+++ 11random.py
@@ -0,0 +1,9 @@
+import random
+
+eded = random.randint(1,6)
+
+print( eded )
+
+#eded = random.random()     0 dan 1e kimi ededleri verir 1 vermir
+#eded = random.random( * 100 )
+#eded = random.choise()     listin icerisinden random deyer secmek ucun
