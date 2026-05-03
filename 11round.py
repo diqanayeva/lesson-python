afbac979aaea012d501f4af386c5377e751f6d6d
diqanayeva <diqanayevaayka@gmail.com>
2026-05-03 12:30:59 +0400

    round

---
+++ 11round.py
@@ -0,0 +1,14 @@
+#print(  round(4.3))
+
+#a = round(  4.6  )
+
+#paint(  a  )
+
+#print( round(  abs(  float(   input( 'Bir eded daxil edin'  )  )  )  ) )
+
+a = input( 'Bir eded daxil edilir')
+b = float( a )
+c = abs( b )
+d = round( c )
+
+print( d )
