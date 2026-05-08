04d7f8b118cfe13994eead9fc166d3263b9d08b4
diqanayeva <diqanayevaayka@gmail.com>
2026-05-08 23:10:17 +0400

    mesaj11

---
+++ m11.py
@@ -0,0 +1,21 @@
+import mesaj11
+
+def hello():
+    print("Hello from m11.py!")
+
+hello()
+
+mesaj11.hello()
+
+
+#from mesaj11 import hello1, hello2
+#from mesaj11 import * 
+
+#def hello():
+     #print("Hello from m11.py!")
+
+#hello()
+
+
+#hello1()
+#hello2()
